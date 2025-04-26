b24b4cbcba615bee712989eccb43a0721ab4da90
il28 <penrouz@inbox.ru>
2025-04-26 20:37:32 +1200

    change function

--- test2.py
+++ test2.py
@@ -77,11 +77,13 @@ def menu():
           exit()
      elif vibor == "Search":
           search()
+     elif vibor == "Change":
+          change()
 
 def allcards():
     message = ""
     for cards_id, cards_info in cards.items():
-        message += f"Card ID: {cards_id}\n" 
+        message += f"Name: {cards_id}\n" 
         for key, value in cards_info.items():
             message += f" {key}: {value}\n" 
     easygui.msgbox(message, "All cards")
@@ -109,6 +111,42 @@ def delete():
                  menu()
             elif after_del == "Cards":
                  allcards()
+
+
+def change():
+        while menu:
+            change_characteristic_id = easygui.enterbox("Please write name of card you want to change.")
+            if change_characteristic_id not in cards:
+                easygui.msgbox("There's no such ID!")
+            else:
+                break #breaks if user entered ID that doesnt exist in the dictionary
+
+
+        change_int_or_str = easygui.buttonbox("You want to change the name or stats?", "???", choices= ["Name", "Stats"])
+             #asks user what exactly he wants to change. I made it this way because it was easier for me to work separately either with .enterbox or .integerbox
+        if change_int_or_str == "Name":
+            new_name = easygui.enterbox("Enter new name.")
+            cards[change_characteristic_id]= new_name #changes value of name to users input
+            menu()
+        elif change_int_or_str == "stats": #if user decided to change stats of the card
+            while True:
+                change_characteristic = easygui.enterbox("Please write what characteristics you want to change.") #asks what kind of stat user wants to change, for example speed, stealth
+                if change_characteristic not in cards[change_characteristic_id]:
+                     easygui.msgbox("No such characteristic!") #if user entered something that doesnt exist in the dictionary, for example "Power" or "Intellect"
+                elif change_characteristic in cards[change_characteristic_id]:
+                    new_characteristic = easygui.integerbox("Please enter new characteristic stat") #asking new value of devided characteristics. For example if speed was 1 and user wants make it 25
+                    if new_characteristic < 1: #if entered stat doesnt required to minimum amount
+                        easygui.msgbox("Too short. Minimum data is - 1.")
+                    elif new_characteristic > 25:
+                        easygui.msgbox("Too much. Maximum is 25.")
+                    else:
+                        cards[change_characteristic_id][change_characteristic] = new_characteristic #changes value of key to users value
+                        break
+
+
+
+
+
 
 def exit():
      exit
